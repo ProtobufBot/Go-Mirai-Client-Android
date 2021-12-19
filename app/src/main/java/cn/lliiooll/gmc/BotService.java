7f122601b52692c31d25bb12ea48dacb884e8fcd
lz1998 <875543533@qq.com>
2021-12-19 19:58:12 +0800

    use chdir

--- app/src/main/java/cn/lliiooll/gmc/BotService.java
+++ app/src/main/java/cn/lliiooll/gmc/BotService.java
@@ -7,11 +7,14 @@ import android.content.ServiceConnection;
 import android.os.Build;
 import android.os.IBinder;
 import android.widget.Toast;
+
 import androidx.annotation.Nullable;
+
 import cn.lliiooll.gmc.util.NotificationUtil;
 import gmc_android.Gmc_android;
 
 import net.lz1998.gomiraiclient.MainActivity;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,9 +28,11 @@ public class BotService extends Service {
     @Override
     public void onCreate() {
         Toast.makeText(this, "GMC启动中...", Toast.LENGTH_LONG).show();// 发个通知别让用户以为程序卡了
-        Gmc_android.setPluginPath(new File(getFilesDir(),"plugins").getAbsolutePath());
-        Gmc_android.setLogPath(new File(getFilesDir(),"logs").getAbsolutePath());
-        Gmc_android.setDevicePath(new File(getFilesDir(),"device").getAbsolutePath());
+        File gmcPath = new File(getFilesDir(), "gmc");
+        if (!gmcPath.exists()) {
+            gmcPath.mkdirs();
+        }
+        Gmc_android.chdir(gmcPath.getAbsolutePath()); // 修改golang工作目录
         Gmc_android.setLogger(s -> {
             // TODO print log on screen
         });
